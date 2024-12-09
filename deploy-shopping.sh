024ca80689960b15b2525b96b73ce85cb124ff3b
donkey <khansession55@gmail.com>
2024-12-09 15:42:10 +0000

    git

--- deploy-shopping.sh
+++ deploy-shopping.sh
@@ -4,7 +4,7 @@ ssh-add ~/.ssh/id_ed25519_jenkins
 ssh -vT git@github.com
 cd shopping-cart-spring-boot-main
 git reset --hard
-git pull
+git pull origin main
 docker builder  prune -f
 docker compose stop -t 1 shopping
 docker compose rm -f shopping
